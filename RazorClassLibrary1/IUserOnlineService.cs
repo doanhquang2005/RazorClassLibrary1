b819c9cbe1febe689edced4db6a76fe1dc133896
Nam Hoàng <namhoang1811@hotmail.com>
2022-03-30 13:54:17 +0700

    Add project files.

---
+++ RazorClassLibrary1/IUserOnlineService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary1
+{
+
+  public interface IUserOnlineService {
+
+    Task Online(ClaimsPrincipal user, bool isOnline = true);
+  }
+}
